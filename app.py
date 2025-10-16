772a36968fb11a192949f7e61a7c5d80dfcf8ed3
Antonio Luis Espinar Chamorro <51887341+nonoespi@users.noreply.github.com>
2025-10-16 09:25:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -838,8 +838,8 @@ if st.session_state.get("finalizado", False):
         # Resuelve filtros estándar (por si no es 'general')
         bu_filter, mm_filter = _resolver_filtros_desde_estado()
 
-		st.caption(f"bu_filter = {bu_filter}")
-		st.caption(f"mm_filter = {mm_filter}")		
+        st.caption(f"bu_filter = {bu_filter}")
+        st.caption(f"mm_filter = {mm_filter}")	
 
         if OFFLINE:
             # ---------- OFFLINE (Parquet) ----------
@@ -1312,6 +1312,7 @@ with header_ph.container():
 
 
     """, unsafe_allow_html=True)
+
 
 
 
